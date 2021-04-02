76d648005bb2398d1943ab701f043ee677f8f3b9
Pedro Mendes <pedrolopesme@gmail.com>
2021-04-02 15:36:28 -0300

    minor: avoid elements repetition on cache

--- main.go
+++ main.go
@@ -6,14 +6,14 @@ import (
 )
 
 type LRUCache struct {
-	MaxSize int64
-	Cache   map[string]*list.Element
-	Recency *list.List
+	maxSize int
+	cache   map[string]*list.Element
+	recency *list.List
 }
 
 // returns a cached element or nil if it wans't found
 func (c *LRUCache) Get(key string) interface{} {
-	element, contains := c.Cache[key]
+	element, contains := c.cache[key]
 	if !contains {
 		return nil
 	}
@@ -23,33 +23,45 @@ func (c *LRUCache) Get(key string) interface{} {
 
 // adds or updates cache with a new element
 func (c *LRUCache) Set(key string, val interface{}) {
-	ele := c.Recency.PushFront(val)
-	c.Cache[key] = ele
+	var ele *list.Element
+	if oldEle, exists := c.cache[key]; exists {
+		ele = c.moveFront(oldEle)
+	} else {
+		ele = c.recency.PushFront(val)
+	}
+	ele.Value = val
+	c.cache[key] = ele
 }
 
 // returns the actual cache size
-func (c *LRUCache) CurrentSize() int64 {
-	return int64(len(c.Cache))
+func (c *LRUCache) CurrentSize() int {
+	return len(c.cache)
 }
 
 // internal func to help on tests. It is not design to
 // use outside testing.
 func (c *LRUCache) valuesToSlice() (out []interface{}) {
-	for element := c.Recency.Front(); element != nil; element = element.Next() {
+	for element := c.recency.Front(); element != nil; element = element.Next() {
 		out = append(out, element.Value)
 	}
 	return
 }
 
+// internal func to move an elemento to the front of recency Queue
+func (c *LRUCache) moveFront(ele *list.Element) *list.Element {
+	c.recency.Remove(ele)
+	return c.recency.PushFront(ele.Value)
+}
+
 // returns a properly initialized LRUCache instance
-func NewLRUCache(size int64) (*LRUCache, error) {
+func NewLRUCache(size int) (*LRUCache, error) {
 	if size < 1 {
 		return nil, errors.New("LRUCache: size must a positive integer")
 	}
 
 	return &LRUCache{
-		MaxSize: size,
-		Cache:   make(map[string]*list.Element, size),
-		Recency: list.New(),
+		maxSize: size,
+		cache:   make(map[string]*list.Element, size),
+		recency: list.New(),
 	}, nil
 }
--- main_test.go
+++ main_test.go
@@ -7,13 +7,13 @@ import (
 )
 
 func TestLRUCache_WhenItsInitializedWithSize_ThenShouldRespectTheGivenSize(t *testing.T) {
-	expectedSize := int64(100)
+	expectedSize := 100
 	cache, _ := NewLRUCache(expectedSize)
-	assert.Equal(t, expectedSize, cache.MaxSize)
+	assert.Equal(t, expectedSize, cache.maxSize)
 }
 
 func TestLRUCache_WhenCacheSizeIsReached_ThenShouldDropExtraElements(t *testing.T) {
-	expectedSize := int64(2)
+	expectedSize := 2
 	cache, _ := NewLRUCache(expectedSize)
 	cache.Set("one", 1)
 	cache.Set("two", 2)
@@ -22,23 +22,34 @@ func TestLRUCache_WhenCacheSizeIsReached_ThenShouldDropExtraElements(t *testing.
 }
 
 func TestLRUCache_WhenSetsAnElement_ThenItShouldBeAbleToGetThatElement(t *testing.T) {
-	expectedSize := int64(2)
+	expectedSize := 2
 	cache, _ := NewLRUCache(expectedSize)
 	cache.Set("one", 1)
 	assert.Equal(t, 1, cache.Get("one"))
 }
 
 func TestLRUCache_WhenGetsAnElementNotInCache_ThenItShouldReturnNil(t *testing.T) {
-	expectedSize := int64(2)
+	expectedSize := 2
 	cache, _ := NewLRUCache(expectedSize)
 	assert.Equal(t, nil, cache.Get("one"))
 }
 
 func TestLRUCache_WhenMultipleElementsAreSetInCache_ThenItShouldStoreThemInLifoOrder(t *testing.T) {
-	expectedSize := int64(2)
+	expectedSize := 2
 	cache, _ := NewLRUCache(expectedSize)
 	cache.Set("c", 3)
 	cache.Set("b", 2)
 	cache.Set("a", 1)
 	assert.Equal(t, []interface{}{1, 2, 3}, cache.valuesToSlice())
+}
+
+func TestLRUCache_WhenSetTheSameElementMultipleTimes_ThenItKeepOnlyTheLastVersion(t *testing.T) {
+	expectedSize := 2
+	cache, _ := NewLRUCache(expectedSize)
+	cache.Set("a", 3)
+	cache.Set("a", 2)
+	cache.Set("a", 1)
+	assert.Equal(t, 1, cache.CurrentSize())
+	assert.Equal(t, 1, cache.recency.Len())
+	assert.Equal(t, 1, cache.Get("a"))
 }
