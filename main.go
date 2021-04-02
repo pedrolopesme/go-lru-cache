313ea62d8960cda7de4931ec3a62eb1942680976
Pedro Mendes <pedrolopesme@gmail.com>
2021-04-02 13:06:30 -0300

    feat: allow to get / set elements to the cache

--- main.go
+++ main.go
@@ -1,18 +1,46 @@
 package main
 
+import (
+	"container/list"
+	"errors"
+)
+
 type LRUCache struct {
-	Size int64
+	MaxSize int64
+	Cache   map[string]interface{}
+	Recency *list.List
 }
 
-// TODO implement
-func (c *LRUCache) Get(key string) (val string) {
-	return
+// returns a cached element or nil if it wans't found
+func (c *LRUCache) Get(key string) interface{} {
+	element, contains := c.Cache[key]
+	if !contains {
+		return nil
+	}
+
+	return element
 }
 
-// TODO implement
-func (c *LRUCache) Set(key, val string) {
+// adds or updates cache with a new element
+func (c *LRUCache) Set(key string, val interface{}) {
+	c.Recency.PushBack(key)
+	c.Cache[key] = val
 }
 
-func NewLRUCache(size int64) *LRUCache {
-	return &LRUCache{Size: size}
+// returns the actual cache size
+func (c *LRUCache) CurrentSize() int64 {
+	return int64(len(c.Cache))
+}
+
+// returns a properly initialized LRUCache instance
+func NewLRUCache(size int64) (*LRUCache, error) {
+	if size < 1 {
+		return nil, errors.New("LRUCache: size must a positive integer")
+	}
+
+	return &LRUCache{
+		MaxSize: size,
+		Cache:   make(map[string]interface{}, size),
+		Recency: list.New(),
+	}, nil
 }
--- main_test.go
+++ main_test.go
@@ -8,6 +8,22 @@ import (
 
 func TestLRUCache_WhenItsInitializedWithSize_ThenShouldRespectTheGivenSize(t *testing.T) {
 	expectedSize := int64(100)
-	cache := NewLRUCache(expectedSize)
-	assert.Equal(t, expectedSize, cache.Size)
+	cache, _ := NewLRUCache(expectedSize)
+	assert.Equal(t, expectedSize, cache.MaxSize)
+}
+
+func TestLRUCache_WhenCacheSizeIsReached_ThenShouldDropExtraElements(t *testing.T) {
+	expectedSize := int64(2)
+	cache, _ := NewLRUCache(expectedSize)
+	cache.Set("one", 1)
+	cache.Set("two", 2)
+	cache.Set("three", 3)
+	assert.Equal(t, expectedSize, cache.CurrentSize())
+}
+
+func TestLRUCache_WhenSetsAnElement_ThenItShouldBeAbleToGetThatElement(t *testing.T) {
+	expectedSize := int64(2)
+	cache, _ := NewLRUCache(expectedSize)
+	cache.Set("one", 1)
+	assert.Equal(t, 1, cache.Get("one"))
 }
